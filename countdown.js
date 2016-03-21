3ba31f665913f343622658536f34f5d991435681
Weston <wan1@rice.edu>
2016-03-21 14:53:53 -0500

    added two state three line header with counter overtime change, and some bug fixes

--- countdown.js
+++ countdown.js
@@ -1,4 +1,9 @@
-var event = "McKinney & Olive Substantial Completion";
+var preface = "Time Remaining to";
+var prefacePost = "Time after Contracted";
+var event1 = "McKinney & Olive";
+var event1Post = event1;
+var event2 = "Substantial Completion";
+var event2Post = event2;
 var year = 2016;
 var month = 2; // starts at 0
 var day = 22;
@@ -17,6 +22,9 @@ var Unit = React.createClass({
 var Count = React.createClass({
     render: function() {
         var classes = "count " + this.props.size
+        if (this.props.overtime) {
+            classes += " overtime"
+        }
         return (
             <div className={classes}>{this.props.count}</div>
         )
@@ -27,7 +35,7 @@ var CountSection = React.createClass({
     render: function() {
         return (
             <div className="col-quarter">
-                <Count count={this.props.count} size={this.props.size}/>
+                <Count count={this.props.count} size={this.props.size} overtime={this.props.overtime}/>
                 <Unit name={this.props.unit} />
             </div>
         )
@@ -37,9 +45,9 @@ var CountSection = React.createClass({
 var DateDisplay = React.createClass({
     render: function() {
         var twelve = this.props.hour >= 12 ? "PM" : "AM";
-        var hour = this.props.hour == 0 ? 12 : this.props.hour % 12;
-        var minute = this.props.minute == 0 ? "00" : this.props.minute;
-        var second = this.props.second == 0 ? "00" : this.props.second;
+        var hour = this.props.hour == 0 ? 12 : this.props.hour % 12 + 1;
+        var minute = this.props.minute < 10 ? "0" + this.props.minute : this.props.minute;
+        var second = this.props.second < 10 ? "0" + this.props.second : this.props.second;
         var time = hour + this.props.minute + this.props.second == 0 ? "":
             <span>at {hour}:{minute}:{second} {twelve}</span>;
         var month = [];
@@ -61,11 +69,43 @@ var DateDisplay = React.createClass({
             </div>
         )
     }
-})
+});
+
+var Header = React.createClass({
+    updateHeader: function() {
+        this.setState({preface: this.props.prefacePost, event1: this.props.event1Post, event2: this.props.event2Post})
+    },
+    getInitialState: function() {
+        return {preface: this.props.preface, event1: this.props.event1, event2: this.props.event2}
+    },
+    render: function() {
+        return (
+            <div className="header">
+                <HeaderLine text={this.state.preface} size={"preface"}/>
+                <HeaderLine text={this.state.event1} size={"event"}/>
+                <HeaderLine text={this.state.event2} size={"event"} />
+            </div>
+        )
+    }
+});
+
+var HeaderLine = React.createClass({
+    render: function() {
+        return (
+            <div className={this.props.size}>{this.props.text}</div>
+        )
+    }
+});
 
 var Counter = React.createClass({
     updateTime: function() {
+        this.setState({overtime: false})
         var diff = timeTill(this.props.year, this.props.month, this.props.day, this.props.hour, this.props.minute, this.props.second);
+        if (diff < 0) {
+            diff = timeSince(this.props.year, this.props.month, this.props.day, this.props.hour, this.props.minute, this.props.second);
+            this.setState({overtime: true})
+            headerComp.updateHeader();
+        }
         this.setState({dayCount: daysTill(diff), hourCount: hoursTill(diff), minuteCount: minsTill(diff), secondCount: secsTill(diff)})
     },
     getInitialState: function() {
@@ -86,10 +126,10 @@ var Counter = React.createClass({
         var daySize = "lg";
         return (
             <div>
-                <CountSection count={this.state.dayCount}    unit={dayUnit}    size={daySize}/>
-                <CountSection count={this.state.hourCount}   unit={hourUnit}   size={hourSize}/>
-                <CountSection count={this.state.minuteCount} unit={minuteUnit} size={minuteSize}/>
-                <CountSection count={this.state.secondCount} unit={secondUnit} size={secondSize}/>
+                <CountSection count={this.state.dayCount}    unit={dayUnit}    size={daySize}    overtime={this.state.overtime}/>
+                <CountSection count={this.state.hourCount}   unit={hourUnit}   size={hourSize}   overtime={this.state.overtime}/>
+                <CountSection count={this.state.minuteCount} unit={minuteUnit} size={minuteSize} overtime={this.state.overtime}/>
+                <CountSection count={this.state.secondCount} unit={secondUnit} size={secondSize} overtime={this.state.overtime}/>
                 <DateDisplay year={this.props.year} month={this.props.month} day={this.props.day}
                              hour={this.props.hour} minute={this.props.minute} second={this.props.second} />
             </div>
@@ -117,15 +157,22 @@ function secsTill(milli) {
 function timeTill(year, month, day, hour, minute, second) {
     var end = new Date(year, month, day, hour+1, minute, second);
     var now = new Date();
-    return end - now ;
+    return end - now;
 }
+
+function timeSince(year, month, day, hour, minute, second) {
+    var end = new Date(year, month, day, hour+1, minute, second);
+    var now = new Date();
+    return now - end;
+}
+
+var headerComp = ReactDOM.render(
+    <Header preface={preface} event1={event1} event2={event2}
+            prefacePost={prefacePost} event1Post={event1Post} event2Post={event2Post}/>,
+    document.getElementById('header')
+);
 
 ReactDOM.render(
     <Counter pollInterval={500} year={year} month={month} day={day} hour={hour} minute={minute} second={second}/>,
     document.getElementById('container')
 );
-
-ReactDOM.render(
-    <span>{event}</span>,
-    document.getElementById('event')
-)
